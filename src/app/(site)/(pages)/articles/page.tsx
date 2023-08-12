e37ae5ba3999bdba54e28a473b8b893cdbd25d86
SilverioGonzalez <gonzalez.silverio7@gmail.com>
2023-08-12 06:53:14 -0400

    padding test

--- src/app/(site)/(pages)/articles/page.tsx
+++ src/app/(site)/(pages)/articles/page.tsx
@@ -5,7 +5,7 @@ import FeatruedArticles from "../../components/featuredArticles";
 
 export default function ArticlePage () {
     return (
-        <main className="min-h-screen p-24">
+        <main className="min-h-screen sm:px-4 lg:p-24">
             <div>
                 <div>
                     <h1 className="text-5xl font-bold md:text-8xl lg:text-9xl">
