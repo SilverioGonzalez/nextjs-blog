408d5d707473b477868082aa56960337a94e8ef9
SilverioGonzalez <gonzalez.silverio7@gmail.com>
2023-08-12 11:58:51 -0400

    updating image for featured projects

--- src/app/(site)/components/featuredProjectBlock.tsx
+++ src/app/(site)/components/featuredProjectBlock.tsx
@@ -18,6 +18,7 @@ export default async function FeaturedProjects (){
                 <div key={project._id}>
                     <Link href={`/projects/${project.slug.current}`} prefetch>
                         <div className="border h-full border-gray-600 hover:scale-105 align-bottom duration-300 hover:shadow-lg hover:shadow-violet-400">
+                            <img src={urlFor(project.coverImage).width(800).height(400).url()} />
                             <div className="p-8">
                                 <h3 className="text-3xl font-bold">{project.title}</h3>
                                 <p className="tracking-tight">{project.shortDescription}</p>
